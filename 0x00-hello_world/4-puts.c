40394b0721b06025eed2519e81b5ef04f96a2ebe
Masayaelvin <masayaelvin@gmail.com>
2022-10-16 03:24:39 -0700

    laptop destroyed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main Entry -Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{	
+
+	puts("\"programmming is like building a multilingual puzzle");
+	return (0);
+
+
+}
