5ba43b51ed4e960b56c49f75e07f9e39e89ea44b
Masayaelvin <masayaelvin@gmail.com>
2022-10-17 07:32:25 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+
 /**
- * main Entry -Point
  *
- * Return: Always 0 (success)
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Sucess)
+ *
  */
+
 int main(void)
-{	
 
-	puts("\"programmming is like building a multilingual puzzle");
-	return (0);
+{
 
+			puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
 
 }
