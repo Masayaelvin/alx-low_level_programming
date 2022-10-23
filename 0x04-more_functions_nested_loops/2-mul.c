77ef5783791e0db1c0e51f904bacaa132803ca59
Masayaelvin <masayaelvin@gmail.com>
2022-10-23 02:58:13 -0700

    0x04 more  loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul multiplies integers
+ *@a is first integer
+ *@b is second integer
+ *
+ *Return addition of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
