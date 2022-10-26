449e817d142fb13351528288e76aae1f7e94aaf4
Masayaelvin <masayaelvin@gmail.com>
2022-10-26 00:49:54 -0700

    0x05 pointes and arrays

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,10 +2,10 @@
 
 /**
  * _putchar - writes character c to stdout
- * @; char to be printed
+ * @: char to be printed
  * Return: On success 1
  */
 int _putchar(char c)
 {
-	return (write(1. &c, 1));
+	return (write(1, &c, 1));
 }
