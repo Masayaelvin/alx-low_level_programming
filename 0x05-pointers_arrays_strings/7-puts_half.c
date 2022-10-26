1b6add55367f0b051b56141e4198fa0a967b86f5
Masayaelvin <masayaelvin@gmail.com>
2022-10-26 01:51:18 -0700

    0x05 pointes and arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half of a string
+ *@str: input
+ */
+
+void puts_half(char *str)
+{
+	int len;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+
+	_putchar('\n');
+}
