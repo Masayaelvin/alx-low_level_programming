6c4d8b03809b82b5ec313bbde83faa9007c3b80e
Masayaelvin <masayaelvin@gmail.com>
2022-11-03 03:04:11 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: is the string to be printed
+ *
+ * Return: Always on success 1
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_puchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (!s)
+		_putchar('\n');
+}
