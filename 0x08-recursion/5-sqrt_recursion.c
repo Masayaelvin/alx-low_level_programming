3888570ba51eb910d1ab62aae1083aa43da9e11a
Masayaelvin <masayaelvin@gmail.com>
2022-11-04 23:36:15 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _increasevar - increases i to find sqrt
  * @i: starts at 1
+ *
  * @n: the number
  * Return: i, i + 1, or -1
  */
@@ -17,11 +18,14 @@ int _increasevar(int i, int n)
 		return (-1);
 		return (i);
 }
+
 /**
  * _sqrt_recursion - finds sqrt
  * @n: the number
  * Return: -1, 0, or sqrt
+ *
  */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
