90283cd962ce4b734ee1f73f465499129f90cf43
Masayaelvin <masayaelvin@gmail.com>
2022-11-07 11:25:59 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
