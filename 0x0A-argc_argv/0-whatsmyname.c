8c2beb89b36b417f44241a56ecd770bcea6ca099
Masayaelvin <masayaelvin@gmail.com>
2022-11-07 07:06:28 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: count
+ * @argv: statements
+ * Return : Always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
