7132cdf41b66e6bf03bf72cb36530507017d6152
Masayaelvin <masayaelvin@gmail.com>
2022-11-15 22:47:54 -0800

    0x0C. C - More malloc, free

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - gives dog details.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: always 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
